4ae584cc881a144274761ec4188131536016d508
Jong-in Park <jargonpink@gmail.com>
2015-02-07 13:53:23 +0900

    Update: Send notification to multi users

--- pushover.py
+++ pushover.py
@@ -6,14 +6,16 @@ class Pushover:
 	def sendNotification(self, **kwargs):
 		title = kwargs.get('title', 'empty title')
 		message = kwargs.get('message', 'empty message')
-		payload = {
-			'token': PUSHOVER_API_KEY,
-			'user': PUSHOVER_USER_KEY,
-			'title': title,
-			'message': message
-		}
 
-		r = requests.post(PUSHOVER_API_HOST, data=payload)
+		for userKey in PUSHOVER_USER_KEYS:
+			payload = {
+				'token': PUSHOVER_API_KEY,
+				'user': userKey,
+				'title': title,
+				'message': message
+			}
+
+			r = requests.post(PUSHOVER_API_HOST, data=payload)
 
 if __name__ == "__main__":
 	pushover = Pushover()
