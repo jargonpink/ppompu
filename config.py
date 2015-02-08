65dd0e88d3e6afb02f3355c36ba1fca10af16267
Jong-in Park <jargonpink@gmail.com>
2015-02-08 23:24:05 +0900

    pushover application key changed

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'mysql://gojinn:thinkbig!@34@localhost/ppomppu'
 
 PUSHOVER_API_HOST = 'https://api.pushover.net/1/messages.json'
-PUSHOVER_API_KEY = 'aY4RnsMoqtKvN3JU9gZMoAwYdMUQa3'
+PUSHOVER_API_KEY = 'aijn4hXsR2Dio9HurEHoacgC98mKkK'
 PUSHOVER_GROUP_KEY = 'gt2j9R8s8u7JWTstVxNy5sEQhAeecq'
