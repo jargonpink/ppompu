1dcd10634cf3544d86ceaaf173c9547ec7c38a35
Jong-in Park <jargonpink@gmail.com>
2015-02-08 17:06:07 +0900

    Update: pick content without tag

--- spider.py
+++ spider.py
@@ -12,8 +12,8 @@ class Spider:
 	keywords = ['Band', 'band', '밴드']
 
 	def main(self):
-		for link in self.getPostLinks(self.board_index_url):
-			try:
+		try:
+			for link in self.getPostLinks(self.board_index_url):
 				post = self.postExtractor(link)
 
 				if db.session.query(Post.id).filter(Post.title==post['title'], Post.content==post['content']).count() > 0:
@@ -21,8 +21,8 @@ class Spider:
 				else:
 					print 'Add: ' + post['link']
 					self.addPost(post)
-			except Exception as e:
-				print 'Exception: ' + str(e)
+		except Exception as e:
+			print 'Exception: ' + str(e)
 
 	def addPost(self, post):
 		self.keywordChecker(post)
@@ -33,18 +33,18 @@ class Spider:
 		hasKeyword = False
 
 		for keyword in self.keywords:
-			if keyword in post['content']:
+			if unicode(keyword, 'utf-8') in post['content']:
 				hasKeyword = True
 				break
 
 		if hasKeyword:
 			push = Pushover()
-			push.sendNotification(title=post['title'], message=post['link'])
+			push.sendNotification(title=post['title'], message=post['content'], url=post['link'])
 
 	def getPostLinks(self, url):
 		links = []
 		r = requests.get(url);
-		s = BeautifulSoup(r.content)
+		s = BeautifulSoup(unicode(r.content, 'cp949').encode('utf-8'))
 		rows = s.find_all('tr', class_=re.compile("list[0-1]"));
 	
 		for row in rows:
@@ -60,11 +60,13 @@ class Spider:
 		s = BeautifulSoup(unicode(r.content, 'cp949').encode('utf-8'))
 
 		post['link'] = url
-		post['title'] = s.find('font', class_='view_title2').text
-		post['content'] = ''
+		post['title'] = s.find('font', class_='view_title2').get_text()
+		post['content'] = unicode()
 
-		for text in s.find_all('table', class_='pic_bg'):
-			post['content'] += str(text)
+		for tag in s.find_all('table', class_='pic_bg'):
+			text = tag.get_text().strip()
+			if text:
+				post['content'] += text
 
 		return post
 
